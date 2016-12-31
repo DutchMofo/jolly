1d34d9299041bd7b3d42b9ea4030110d4eeaf7f6
DutchMofo <loovanderpiet@gmail.com>
2016-12-31 15:43:31 +0100

    Reordering

--- Analyser.cs
+++ Analyser.cs
@@ -11,42 +11,68 @@ static class Analyser
 {
 	public static void analyse(List<Node> program)
 	{
+		Action<Node> action;
 		foreach(Node node in program)
 		{
-			if(analysers.ContainsKey(node.nodeType)) {
-				analysers[node.nodeType](node);
+			if(analysers.TryGetValue(node.nodeType, out action)) {
+				action(node);
 			} else {
-				// Jolly.addError(new SourceLocation(), "");
-				// throw new ParseException();
+				Jolly.addError(new SourceLocation(), "");
+				throw new ParseException();
 			}
 		}
 	}
 	
-	static readonly Dictionary<NT, Action<Node>> analysers = new Dictionary<NT, Action<Node>>(){
-		{ NT.OPERATOR,	analyseOperator },
-		{ NT.FUNCTION,	analyseFunction },
-		{ NT.NAME,		analyseName		},
+	static readonly Dictionary<NT, Action<Node>> analysers = new Dictionary<NT, Action<Node>>() {
+		{ NT.ALIAS, node => { } },
+		{ NT.BLOCK, node => { } },
+		{ NT.BREAK, node => { } },
+		{ NT.CAST, node => { } },
+		{ NT.FOR, node => { } },
+		{ NT.FUNCTION, node => { } },
+		{ NT.FUNCTION_CALL, node => { } },
+		{ NT.GOTO, node => { } },
+		{ NT.IF, node => { } },
+		{ NT.IF_ELSE, node => { } },
+		{ NT.LABEL, node => { } },
+		{ NT.TUPPLE, node => { } },
+		{ NT.LITERAL, node => { } },
+		{ NT.LOOP_CONTROL, node => { } },
+		{ NT.NAME, node => {
+			Debug.Assert(false);
+		} },
+		{ NT.OPERATOR, node => {
+			Operator o = (Operator)node;
+			operatorAnalysers[o.operation](o);
+		} },
+		{ NT.RESULT, node => { } },
+		{ NT.RETURN, node => { } },
+		{ NT.STATEMENT, node => { } },
+		{ NT.USING, node => { } },
+		{ NT.VARIABLE_DEFINITION, node => { } },
+		{ NT.VARIABLE_RW, node => { } },
+		{ NT.WHILE, node => { } },
 	};
 	
 	static readonly Dictionary<TT, Action<Operator>> operatorAnalysers = new Dictionary<TT, Action<Operator>>() {
-		{ TT.REFERENCE,		 op => { } },
-		{ TT.DEREFERENCE,	 op => { } },
-		{ TT.PLUS,			 op => { } },
-		{ TT.MINUS,			 op => { } },
-		{ TT.INCREMENT,		 op => { } },
-		{ TT.DECREMENT,		 op => { } },
-		{ TT.LOGIC_AND,		 op => { } },
-		{ TT.EQUAL_TO,		 op => { } },
-		{ TT.LOGIC_OR,		 op => { } },
-		{ TT.LOGIC_NOT,		 op => { } },
-		{ TT.BIT_NOT,		 op => { } },
-		{ TT.BIT_AND,		 op => { } },
-		{ TT.BIT_OR,		 op => { } },
-		{ TT.BIT_XOR,		 op => { } },
-		{ TT.MODULO,		 op => { } },
-		{ TT.DIVIDE,		 op => { } },
-		{ TT.MULTIPLY,		 op => { } },
-		{ TT.GET_MEMBER,	 op => {
+		{ TT.REFERENCE, op => { } },
+		{ TT.DEREFERENCE, op => { } },
+		{ TT.PLUS, op => { } },
+		{ TT.MINUS, op => { } },
+		{ TT.INCREMENT, op => { } },
+		{ TT.DECREMENT,	op => { } },
+		{ TT.LOGIC_AND,	op => { } },
+		{ TT.EQUAL_TO, op => { } },
+		{ TT.LOGIC_OR, op => { } },
+		{ TT.LOGIC_NOT, op => { } },
+		{ TT.BIT_NOT, op => { } },
+		{ TT.BIT_AND, op => { } },
+		{ TT.BIT_OR, op => { } },
+		{ TT.BIT_XOR, op => { } },
+		{ TT.MODULO, op => { } },
+		{ TT.DIVIDE, op => { } },
+		{ TT.MULTIPLY, op => { } },
+		{ TT.GET_MEMBER,op => {
 			if(op.a.dataType == null)
 				getTypeFromName(op.a);
 			
@@ -60,15 +86,15 @@ static class Analyser
 			
 			
 		} },
-		{ TT.SUBSCRIPT,		 op => { } },
-		{ TT.READ,			 op => { } },
-		{ TT.ASSIGN,		 op => { } },
-		{ TT.SHIFT_LEFT,	 op => { } },
-		{ TT.SHIFT_RIGHT,	 op => { } },
-		{ TT.LESS_EQUAL,	 op => { } },
-		{ TT.GREATER_EQUAL,	 op => { } },
-		{ TT.SLICE,			 op => { } },
-		{ TT.CAST,			 op => { } },
+		{ TT.SUBSCRIPT, op => { } },
+		{ TT.READ, op => { } },
+		{ TT.ASSIGN, op => { } },
+		{ TT.SHIFT_LEFT, op => { } },
+		{ TT.SHIFT_RIGHT, op => { } },
+		{ TT.LESS_EQUAL, op => { } },
+		{ TT.GREATER_EQUAL, op => { } },
+		{ TT.SLICE, op => { } },
+		{ TT.CAST, op => { } },
 	};
 	
 	static void getTypeFromName(Node node)
@@ -86,22 +112,6 @@ static class Analyser
 			
 			name.dataType = item.type;
 		}
-	}
-	
-	static void analyseOperator(Node n)
-	{
-		Operator o = (Operator)n;
-		operatorAnalysers[o.operation](o);
-	}
-	
-	static void analyseFunction(Node n)
-	{
-		
-	}
-	
-	static void analyseName(Node n)
-	{
-		Debug.Assert(false);
 	}
 }
 }
